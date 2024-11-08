af6165d3755899ae3718a00ce65316acb25a93a8
itsathefak <athefayubkhan@gmail.com>
2024-11-08 12:01:14 -0500

    Set up mongoose

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://athefak:123qwe@cluster0.qkqxgq2.mongodb.net/whatsapp?retryWrites=true&w=majority"
+    );
+    console.log("Connection successful to the database");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+  }
+}
+
+main();
 app.get("/", (req, res) => {
   res.send("The root is working");
 });
