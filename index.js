eee6c25bb079dd2a03bff875dd4e510f865f415e
itsathefak <athefayubkhan@gmail.com>
2024-11-08 11:44:32 -0500

    set up express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("The root is working");
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
